fe1070cb6d249366e67184e94e1f8d54fea3b549
dwpeng <dwpeng1122@foxmail.com>
2025-01-16 22:59:27 +0800

    feat: 更新安装前提脚本，移除不必要的依赖项并添加构建前清理步骤

--- devtools/install-prerequisites.sh
+++ devtools/install-prerequisites.sh
@@ -3,12 +3,12 @@
 if test -x /usr/bin/dnf; then
     echo Installing prerequisites via dnf...
     dnf -y install epel-release
-    dnf -y install zlib-devel bzip2-devel xz-devel curl-devel openssl-devel samtools bcftools htslib-tools
+    dnf -y install zlib-devel bzip2-devel xz-devel curl-devel openssl-devel
 
 elif test -x /usr/bin/yum; then
     if yum -y install epel-release; then
         echo Installing prerequisites via yum...
-        yum -y install zlib-devel bzip2-devel xz-devel curl-devel openssl-devel samtools bcftools htslib-tools
+        yum -y install zlib-devel bzip2-devel xz-devel curl-devel openssl-devel
     else
         echo Installing non-test prerequisites via yum...
         yum -y install zlib-devel bzip2-devel xz-devel curl-devel openssl-devel
@@ -17,17 +17,12 @@ elif test -x /usr/bin/yum; then
 elif test -d /etc/dpkg; then
     echo Installing prerequisites via apt-get...
     apt-get update
-    apt-get install -y --no-install-recommends --no-install-suggests libcurl4-openssl-dev libssl-dev zlib1g-dev libbz2-dev liblzma-dev samtools bcftools tabix
+    apt-get install -y --no-install-recommends --no-install-suggests libcurl4-openssl-dev libssl-dev zlib1g-dev libbz2-dev liblzma-dev
 
 elif test -x /sbin/apk; then
     echo Installing non-test prerequisites via apk...
     apk update
     apk add zlib-dev bzip2-dev xz-dev curl-dev openssl-dev
-
-elif test -x ${HOMEBREW_PREFIX-/usr/local}/bin/brew; then
-    echo Installing prerequisites via brew...
-    HOMEBREW_NO_AUTO_UPDATE=1 brew install -q samtools bcftools
-    brew unlink xz || true
 
 else
     echo No package manager detected
